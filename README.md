8104548cc7e68b7531c59d8916160a3844daa13a
hanepjiv <hanepjiv@gmail.com>
2018-06-22 09:48:34 +0900

    Fixes for rustc 1.27.0 (3eda71b00 2018-06-19)

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 ## build status
 - master: [![Build Status](https://travis-ci.org/hanepjiv/slide_cast-rs.svg?branch=master)](https://travis-ci.org/hanepjiv/slide_cast-rs)
+
+## License
+
+Licensed under either of
+
+ * Apache License, Version 2.0
+   ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license
+   ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
--- src/lib.rs
+++ src/lib.rs
@@ -6,31 +6,41 @@
 //  @author hanepjiv <hanepjiv@gmail.com>
 //  @copyright The MIT License (MIT) / Apache License Version 2.0
 //  @since 2016/03/01
-//  @date 2018/04/21
+//  @date 2018/06/22
 
 // ////////////////////////////////////////////////////////////////////////////
 // attribute  =================================================================
+// rustc 1.27.0 (3eda71b00 2018-06-19)
 #![deny(
-    anonymous_parameters, box_pointers, missing_copy_implementations,
-    missing_debug_implementations, missing_docs, trivial_casts,
-    trivial_numeric_casts, unsafe_code, unstable_features,
+    absolute_paths_not_starting_with_crate, anonymous_parameters,
+    bare_trait_objects, missing_copy_implementations,
+    missing_debug_implementations, missing_docs, unstable_features,
     unused_extern_crates, unused_import_braces, unused_qualifications,
-    unused_results, variant_size_differences, const_err, dead_code, deprecated,
+    unused_results, variant_size_differences, const_err, deprecated,
     illegal_floating_point_literal_pattern, improper_ctypes,
-    late_bound_lifetime_arguments, non_camel_case_types,
-    non_shorthand_field_patterns, non_snake_case, non_upper_case_globals,
-    no_mangle_generic_items, overflowing_literals, path_statements,
-    patterns_in_fns_without_body, plugin_as_library, private_in_public,
-    private_no_mangle_fns, private_no_mangle_statics,
-    renamed_and_removed_lints, stable_features, unconditional_recursion,
-    unions_with_drop_fields, unknown_lints, unreachable_code,
-    unreachable_patterns, unused_allocation, unused_assignments,
-    unused_attributes, unused_comparisons, unused_doc_comment, unused_features,
-    unused_imports, unused_macros, unused_must_use, unused_mut, unused_parens,
-    unused_unsafe, unused_variables, while_true
+    incoherent_fundamental_impls, late_bound_lifetime_arguments,
+    non_camel_case_types, non_shorthand_field_patterns, non_snake_case,
+    non_upper_case_globals, no_mangle_generic_items, overflowing_literals,
+    path_statements, patterns_in_fns_without_body, plugin_as_library,
+    private_in_public, private_no_mangle_fns, private_no_mangle_statics,
+    safe_packed_borrows, stable_features, type_alias_bounds,
+    tyvar_behind_raw_pointer, unconditional_recursion, unions_with_drop_fields,
+    unknown_lints, unreachable_code, unreachable_patterns,
+    unstable_name_collisions, unused_allocation, unused_assignments,
+    unused_attributes, unused_comparisons, unused_doc_comments,
+    unused_features, unused_imports, unused_macros, unused_must_use,
+    unused_mut, unused_parens, unused_unsafe, unused_variables, while_true,
+    exceeding_bitshifts, invalid_type_param_default,
+    legacy_constructor_visibility, legacy_directory_ownership, legacy_imports,
+    missing_fragment_specifier, mutable_transmutes, no_mangle_const_items,
+    parenthesized_params_in_types_and_modules, pub_use_of_private_extern_crate,
+    safe_extern_statics, unknown_crate_types
 )]
-#![warn(dead_code)]
-#![allow(box_pointers, unsafe_code, trivial_casts, trivial_numeric_casts)]
+#![warn(dead_code, deprecated, renamed_and_removed_lints, unreachable_pub)]
+#![allow(
+    box_pointers, elided_lifetimes_in_paths, single_use_lifetimes,
+    trivial_casts, trivial_numeric_casts, unsafe_code
+)]
 // ////////////////////////////////////////////////////////////////////////////
 // ============================================================================
 #[macro_export]
